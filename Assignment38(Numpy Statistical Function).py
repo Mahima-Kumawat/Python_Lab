b1f0777e8cb6af5efe1724d8549836b96a9f95c7
Mahima-Kumawat <74977089+Mahima-Kumawat@users.noreply.github.com>
2024-07-24 20:06:06 +0530

    Add files via upload

---
+++ Assignment38(Numpy Statistical Function).py
@@ -0,0 +1,25 @@
+#Compute the median of the flattened NumPy array 
+
+
+import numpy as np
+
+# Creating an array with odd number
+x_odd = np.array([1, 2, 3, 4, 5, 6, 7])
+print("Original Array:")
+print(x_odd)
+
+# Calculating median
+median_value = np.median(x_odd)
+print("Median of the array:")
+print(median_value)
+
+
+#Output=>
+
+Original Array:
+[1 2 3 4 5 6 7]
+Median of the array:
+4.0
+
+
+
---
+++ Assignment39(Numpy Statistical Function).py
@@ -0,0 +1,21 @@
+# Compute the standard deviation of the NumPy array
+
+
+import numpy as np
+
+#1D Array
+arr = [20, 2, 7, 1, 34]
+print("Array:", arr)
+
+std_dev = np.std(arr)
+print("Standard deviation of the array:", std_dev)
+
+
+# Output=>
+
+Array: [20, 2, 7, 1, 34]
+Standard deviation of the array: 12.576167937809991
+
+
+
+
