8486e7649530f81f99ee82ec558c073923e34840
Mahima-Kumawat <74977089+Mahima-Kumawat@users.noreply.github.com>
2024-07-30 16:26:03 +0530

    Add files via upload

---
+++ Assignment41(Numpy mathematical operation).py
@@ -0,0 +1,23 @@
+# Calculate the profit made by a company.
+
+
+import numpy as np
+
+# Define the revenue and expenses arrays
+revenue = np.array([10000, 12000, 11000, 10500])
+expenses = np.array([4000, 5000, 4500, 4800])
+
+# Calculate the profit
+profit = revenue - expenses
+
+# Print the profit
+print("Profit:", profit)
+
+
+# Output=>
+
+Profit: [6000 7000 6500 5700]
+
+
+
+
